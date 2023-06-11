2e2a97dee967f1f7b822997e43051b7cbaf8f8ac
wdawadwda <wdawadwdawork@gmail.com>
2023-06-11 15:26:56 +0300

    feat: component buton
    feat: fonts
    added bebasNeue font via cdn and downloaded and added helios font to the
    project

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
-import './App.styles.css';
+import './App.styles.scss';
 
 export const App = () => {
-  return <h1>Getting started</h1>;
+  return (
+    <>
+      <div>Getting started</div>
+    </>
+  );
 };
