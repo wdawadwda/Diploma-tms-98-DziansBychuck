a58af8f408210108eb2eaf90f33fa18fe6cda891
wdawadwda <wdawadwdawork@gmail.com>
2023-06-12 14:57:43 +0300

    fix: footer

--- src/features/Footer/footer.tsx
+++ src/features/Footer/footer.tsx
@@ -1,7 +1,10 @@
+import Style from './footer.module.scss';
+
 export const Footer = () => {
   return (
-    <>
-      <div></div>
-    </>
+    <div className={Style.container}>
+      <span>©2022 Bookstore</span>
+      <span>All rights reserved</span>
+    </div>
   );
 };
