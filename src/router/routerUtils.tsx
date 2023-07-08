e27dfba5b6c78d0044d5df966ea82a2cdd9366c8
wdawadwda <wdawadwdawork@gmail.com>
2023-07-08 21:50:08 +0300

    refactor: combined the functions
    protectedRoute and publicOnlyRoute into a new function accessControlRoute

--- src/router/routerUtils.tsx
+++ src/router/routerUtils.tsx
@@ -3,30 +3,6 @@ import { Navigate, Outlet } from 'react-router-dom';
 
 import { selectUser } from '~/store/user/user.selectors';
 
-export const ProtectedRoute = () => {
-  const user = useSelector(selectUser);
-  return user ? (
-    <Outlet />
-  ) : (
-    <Navigate
-      to="/"
-      replace
-    />
-  );
-};
-
-export const PublicOnlyRoute = () => {
-  const user = useSelector(selectUser);
-  return user ? (
-    <Navigate
-      to="/"
-      replace
-    />
-  ) : (
-    <Outlet />
-  );
-};
-
 export const AccessControlRoute = ({
   isProtected = false,
   isPublicOnly = false
