31ef2458a410e60476a53b2b272f3e02a020072f
wdawadwda <wdawadwdawork@gmail.com>
2023-07-13 21:59:11 +0300

    fix: themeSlice, themeListeners, store
    implemented saving theme to localStorage and changing theme to light
    upon logout

--- src/store/theme/theme.slice.ts
+++ src/store/theme/theme.slice.ts
@@ -10,12 +10,25 @@ const getInitialState = (): ThemeState => ({
 
 export const themeSlice = createSlice({
   name: 'theme',
-  initialState: getInitialState,
+  initialState: getInitialState(),
   reducers: {
     changeTheme: (state, action: PayloadAction<Mode>) => {
       state.mode = action.payload;
       state.isDark = action.payload === 'dark';
       state.isLight = action.payload === 'light';
+      localStorage.setItem('themeMode', action.payload);
+    },
+    setThemeFromLocalStorage: (state) => {
+      const themeMode = localStorage.getItem('themeMode');
+      if (themeMode === 'dark') {
+        state.mode = 'dark';
+        state.isDark = true;
+        state.isLight = false;
+      } else {
+        state.mode = 'light';
+        state.isDark = false;
+        state.isLight = true;
+      }
     }
   }
 });
